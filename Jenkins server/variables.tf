e68c4c72fb3cd33e1497b659859a37159fd24f03
padmavathi kumbha <padmavathik2637@gmail.com>
2023-10-21 00:23:17 -0400

    Adding files to repo

---
+++ Jenkins server/variables.tf
@@ -0,0 +1,13 @@
+variable "vpc_cidr"{
+    description = "VPC CIDR"
+    type = string
+}
+variable "public_subnets"{
+  description = "subnet CIDR"
+  type        = list(string) 
+}
+
+variable "instance_type"{
+  description = "Instance_Type"
+  type        = string 
+}
